de0d60a6cfd9bffe9e974202ed5fc4cc55d2c514
Anwesh43 <anweshthecool0@gmail.com>
2018-04-02 08:19:21 +0530

    adding VToXView class

--- vtoxview/src/main/java/ui/anwesome/com/vtoxview/VToXView.kt
+++ vtoxview/src/main/java/ui/anwesome/com/vtoxview/VToXView.kt
@@ -6,3 +6,21 @@ package ui.anwesome.com.vtoxview
 import android.content.Context
 import android.graphics.*
 import android.view.*
+
+class VToXView(ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
