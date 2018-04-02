bf2d42a89bad47e49212b9c8e694f75af236d335
Anwesh43 <anweshthecool0@gmail.com>
2018-04-02 08:25:09 +0530

    adding VToXState class

--- vtoxview/src/main/java/ui/anwesome/com/vtoxview/VToXView.kt
+++ vtoxview/src/main/java/ui/anwesome/com/vtoxview/VToXView.kt
@@ -23,4 +23,28 @@ class VToXView(ctx : Context) : View(ctx) {
         }
         return true
     }
+    data class VToXState(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * this.dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += dir.toInt()
+                if (j == scales.size || j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
