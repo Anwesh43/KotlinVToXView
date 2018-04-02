153ec75194ef678e494c356c9f59049ac53a9c2b
Anwesh43 <anweshthecool0@gmail.com>
2018-04-02 08:46:16 +0530

    adding VToXShape class

--- vtoxview/src/main/java/ui/anwesome/com/vtoxview/VToXView.kt
+++ vtoxview/src/main/java/ui/anwesome/com/vtoxview/VToXView.kt
@@ -74,4 +74,31 @@ class VToXView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VToXShape (var i : Int, val state : VToXState = VToXState()) {
+        fun draw(canvas : Canvas, paint : Paint) {
+            val w = canvas.width.toFloat()
+            val h = canvas.height.toFloat()
+            val size : Float  = Math.min(w,h)/5
+            paint.color = Color.parseColor("#9b59b6")
+            paint.strokeWidth = size/11
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.save()
+            canvas.translate(w/2, h/2)
+            for (i in 0..1) {
+                canvas.save()
+                canvas.translate((size/(2 * Math.sqrt(2.0).toFloat())) * (1 -state.scales[1]) * (1 - 2 * i), 0f)
+                canvas.rotate(45f * (1 - state.scales[0] + state.scales[2]) * (1 - 2 * i))
+                canvas.drawLine(0f, -size/2,0f, size/2, paint)
+                canvas.restore()
+            }
+            canvas.restore()
+        }
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+    }
 }
